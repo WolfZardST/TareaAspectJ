e3420de4838a078a9a783695efebad7b219da648
rxcp02 <rxcabrer@espol.edu.ec>
2021-10-31 23:35:22 -0500

    Primer commit

---
+++ src/Ventana.java
@@ -0,0 +1,20 @@
+import java.awt.Panel;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ventana {
+	public static void main(String[] args) {
+		Boton boton=new Boton();
+        boton.setBounds(0,0,350,200);
+        boton.setVisible(true);
+        boton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
+	
+	
+	
+}
